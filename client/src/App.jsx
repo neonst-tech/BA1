d37a0476d3610ad8c724a48e066641478021bd9c
neonst-tech <connect.neonst@gmail.com>
2026-08-21 16:19:41 +0530

    Add starter React app

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Property Rental Management Portal</h1>
+        <p>Starter application. Begin implementation from the Project Requirement Document.</p>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
